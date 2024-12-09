ce91ad5de7f456251b3224cd56993aac61a047e2
Amirsaeed Ahmadi <amirsaeedahmadi80@yahoo.com>
2024-12-09 19:40:15 +0330

    Fix: minor bug

--- user/src/test/java/com/kalado/user/UserServiceIntegrationTest.java
+++ user/src/test/java/com/kalado/user/UserServiceIntegrationTest.java
@@ -17,7 +17,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class UserServiceIntegrationTest {
 
   @Autowired
