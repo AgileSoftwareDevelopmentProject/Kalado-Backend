517cea02ed4438c42f1f255a291c134b8b37b44e
amirsaeedahmadi <amirsaeedahmadi80@yahoo.com>
2024-12-31 01:40:12 +0330

    change: change redis template in authentication configuartion

--- authentication/src/main/java/com/kalado/authentication/AuthenticationConfiguration.java
+++ authentication/src/main/java/com/kalado/authentication/AuthenticationConfiguration.java
@@ -3,21 +3,34 @@ package com.kalado.authentication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class AuthenticationConfiguration<T, J> {
 
-  @Bean
-  public BCryptPasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
-  }
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-  @Bean
-  RedisTemplate<T, J> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-    RedisTemplate<T, J> template = new RedisTemplate<>();
-    template.setConnectionFactory(redisConnectionFactory);
-    return template;
-  }
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        // Configure Redis connection here (default: localhost, port: 6379)
+        return new LettuceConnectionFactory("localhost", 6379);
+    }
+
+    @Bean
+    public RedisTemplate<T, J> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<T, J> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+
+        // Set serializers for better Redis interaction
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+
+        return template;
+    }
 }
