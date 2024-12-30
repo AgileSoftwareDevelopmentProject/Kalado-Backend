5cf805bdf94ca470be0f2a7ba40f740953c6c4ad
amirsaeedahmadi <amirsaeedahmadi80@yahoo.com>
2024-12-30 23:32:03 +0330

    fix: bug in webconfig

--- gateway/src/main/java/com/kalado/gateway/configuration/WebConfig.java
+++ gateway/src/main/java/com/kalado/gateway/configuration/WebConfig.java
@@ -1,12 +1,11 @@
 package com.kalado.gateway.configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import feign.codec.ErrorDecoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,22 +14,48 @@ public class WebConfig implements WebMvcConfigurer {
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000", "https://kalado.app/")
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
-            .allowedHeaders("*")
-            .exposedHeaders("*")
-            .allowCredentials(true)
-            .maxAge(3600);
+    registry
+        // .addMapping("/**")
+
+        // // .allowedOrigins("*")
+        // .allowedOrigins("http://localhost:8080", "http://kaladoshop.com")
+
+
+
+        // .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+
+        // .allowedHeaders("*")
+
+        // .allowCredentials(true)
+
+        // .maxAge(3600);
+      .addMapping("/**")
+
+      .allowedOrigins(
+          "http://localhost:8080",  
+          "http://kaladoshop.com/"
+      )
+
+      .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+
+      .allowedHeaders("*")
+
+      .maxAge(3600);
   }
 
+//  @Override
+//  public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+//    configurer.favorParameter(true)
+//            .parameterName("mediaType")
+//            .defaultContentType(MediaType.APPLICATION_JSON)
+//            .mediaType("json", MediaType.APPLICATION_JSON)
+//            .mediaType("multipart", MediaType.MULTIPART_FORM_DATA);
+//  }
+//
+
   @Bean
-  @Primary
   public ObjectMapper objectMapper() {
-    ObjectMapper objectMapper = new ObjectMapper();
-    objectMapper.registerModule(new JavaTimeModule());
-    objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-    return objectMapper;
+    return new ObjectMapper();
   }
 
   @Bean
