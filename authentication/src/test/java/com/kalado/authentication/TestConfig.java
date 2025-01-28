38c1c7240d80471b079e329fa8c8df69d82e51de
Amirhossein Arabzadeh <arabzadehamirhossein888@gmail.com>
2025-01-28 15:10:05 +0330

    add authentication service tests

--- authentication/src/test/java/com/kalado/authentication/TestConfig.java
+++ authentication/src/test/java/com/kalado/authentication/TestConfig.java
@@ -1,5 +1,6 @@
 package com.kalado.authentication;
 
+import com.kalado.authentication.application.service.EmailService;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
@@ -12,6 +13,24 @@ public class TestConfig {
     @Bean
     @Primary
     public JavaMailSender javaMailSender() {
+        // Return a non-functional mail sender for tests
         return new JavaMailSenderImpl();
+    }
+
+    @Bean
+    @Primary
+    public EmailService emailService(JavaMailSender javaMailSender) {
+        // Create a test version of EmailService that doesn't actually send emails
+        return new EmailService(javaMailSender) {
+            @Override
+            public void sendVerificationToken(String to, String token) {
+                // Do nothing in tests
+            }
+
+            @Override
+            public void sendPasswordResetToken(String to, String token) {
+                // Do nothing in tests
+            }
+        };
     }
 }
