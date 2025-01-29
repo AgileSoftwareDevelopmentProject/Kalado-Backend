c8c44769dd1903607c9bfc2d2ae93f038ec01c87
Amirhossein Arabzadeh <arabzadehamirhossein888@gmail.com>
2025-01-29 13:48:42 +0330

    return all the products

--- product/src/main/java/com/kalado/product/application/service/ProductService.java
+++ product/src/main/java/com/kalado/product/application/service/ProductService.java
@@ -146,7 +146,7 @@ public class ProductService {
   @Transactional(readOnly = true)
   public List<Product> getProductsByCategory(String category) {
     return productRepository.findByCategory(category).stream()
-            .filter(product -> product.getStatus() != ProductStatus.DELETED)
+//            .filter(product -> product.getStatus() != ProductStatus.DELETED)
             .collect(Collectors.toList());
   }
 
@@ -201,7 +201,7 @@ public class ProductService {
   @Transactional(readOnly = true)
   public List<Product> getAllProducts() {
     return productRepository.findAll().stream()
-            .filter(product -> product.getStatus() != ProductStatus.DELETED)
+//            .filter(product -> product.getStatus() != ProductStatus.DELETED)
             .collect(Collectors.toList());
   }
 }
