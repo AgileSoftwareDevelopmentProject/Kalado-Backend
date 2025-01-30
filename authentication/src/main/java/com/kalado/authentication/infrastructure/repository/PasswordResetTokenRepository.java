deb65924bfdc508122bb8dd95454ab712b83376a
Amirhossein Arabzadeh <arabzadehamirhossein888@gmail.com>
2025-01-30 08:24:00 +0330

    remove unused code

--- authentication/src/main/java/com/kalado/authentication/infrastructure/repository/PasswordResetTokenRepository.java
+++ authentication/src/main/java/com/kalado/authentication/infrastructure/repository/PasswordResetTokenRepository.java
@@ -6,5 +6,4 @@ import java.util.Optional;
 
 public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
     Optional<PasswordResetToken> findByToken(String token);
-    void deleteByToken(String token);
 }
